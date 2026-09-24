76f1f48c8bb19efe029ebe034af4383d44b950d5
naseer7399 <64818841+naseer7399@users.noreply.github.com>
2026-09-24 08:51:31 -0400

    Update Firebase config with credentials and enable
    Updated Firebase configuration with actual credentials and enabled Firebase.

--- firebase-config.js
+++ firebase-config.js
@@ -99,13 +99,14 @@
    ------------------------------------------------------------------ */
 
 const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+   apiKey: "AIzaSyCUOJ-D_CWS5QFbzX8k69WVUv12KJ_h0Ek",
+   authDomain: "iks-1-4v-db.firebaseapp.com",
+   projectId: "iks-1-4v-db",
+   storageBucket: "iks-1-4v-db.firebasestorage.app",
+   messagingSenderId: "911159075301",
+   appId: "1:911159075301:web:5d620a7914c4f9827f4f0c",
+   measurementId: "G-BPQG3B30WR"
 };
 
 // Flip this to true only after completing the steps above.
-const FIREBASE_ENABLED = false;
+const FIREBASE_ENABLED = true;
